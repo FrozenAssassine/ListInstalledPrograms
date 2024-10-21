d069b09a58bf11e2d7011a1e797151df7fe2e830
Julius Kirsch <julius@skirsch.de>
2024-10-21 08:57:51 +0200

    feat: another way

--- getInstalled.ps1
+++ getInstalled.ps1
@@ -1,67 +1,109 @@
-# Get lists from the registry
-$installedAppsHKLM = Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" |
-Select-Object DisplayName, DisplayVersion, InstallDate, @{Name = "Maintainer"; Expression = { "HKLM" } }
+class InstalledApp {
+    [string]$DisplayName
+    [string]$DisplayVersion
+    [string]$InstallDate
+    [string]$Maintainer
 
-$installedAppsHKCU = Get-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" |
-Select-Object DisplayName, DisplayVersion, InstallDate, @{Name = "Maintainer"; Expression = { "HKCU" } }
-
-$installedAppsHKLMWow = Get-ItemProperty "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | 
-Select-Object DisplayName, DisplayVersion, InstallDate, @{Name = "Maintainer"; Expression = { "HKLM Wow6432Node" } }
-
-#combine lists from registry + remove duplicates
-$combinedApps = $installedAppsHKLM + $installedAppsHKCU + $installedAppsHKLMWow |
-Sort-Object DisplayName -Unique
-
-#run the choco command + parse data into two lists
-$chocoOut = choco list
-$lines = $chocoOut -split "`n"
-#skip the first and last line, because its the headline and end of the output:
-$lines = $lines[1..($lines.Length - 2)]
-
-$processedLines = $lines | ForEach-Object {
-    $remIndex = $_.IndexOf(" ");
-    if ($remIndex -ne -1) {
-        $_.Remove($remIndex).Replace("-", " ").ToLower()
-    }
-    else {
-        $_.Replace("-", " ").ToLower()
+    InstalledApp([string]$displayName, [string]$displayVersion, [string]$installDate, [string]$maintainer) {
+        $this.DisplayName = $displayName
+        $this.DisplayVersion = $displayVersion
+        $this.InstallDate = $installDate
+        $this.Maintainer = $maintainer
     }
 }
 
-$processedLines
-
-$maintainer = "-"
-$allApps = $combinedApps | ForEach-Object {
-    if (![string]::IsNullOrEmpty($_.DisplayName)) {
+class InstalledAppManager {
     
-        $maintainer = "-"
-        foreach ($line in $processedLines) {
+    [InstalledApp[]] getAllAppsFromRegistry() {
 
-            if ($_.DisplayName.ToLower().Replace(" ", "-") -like "*$line*" -or 
-                $_.DisplayName.ToLower().Replace(" ", "") -like "*$line*" -or 
-                $_.DisplayName.ToLower().Replace("-", "") -like "*$line*"
-            ) {
-                $maintainer = "Chocolatey"
-                break;
-            }
+        #get lists from the registry
+        $installedAppsHKLM = Get-ItemProperty "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" |
+        ForEach-Object {
+            [InstalledApp]::new($_.DisplayName, $_.DisplayVersion, $_.InstallDate, "HKLM")
         }
 
-        [PSCustomObject]@{
-            DisplayName    = $_.DisplayName
-            DisplayVersion = $_.DisplayVersion
-            InstallDate    = $_.InstallDate
-            Maintainer     = if ($maintainer -eq "-") { $_.Maintainer } else { $maintainer }
+        $installedAppsHKCU = Get-ItemProperty "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*" |
+        ForEach-Object {
+            [InstalledApp]::new($_.DisplayName, $_.DisplayVersion, $_.InstallDate, "HKCU")
         }
+
+        $installedAppsHKLMWow = Get-ItemProperty "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" |
+        ForEach-Object {
+            [InstalledApp]::new($_.DisplayName, $_.DisplayVersion, $_.InstallDate, "HKLM Wow6432Node")
+        }
+
+        #combine lists from registry + remove duplicates
+        return $installedAppsHKLM + $installedAppsHKCU + $installedAppsHKLMWow |
+        Sort-Object DisplayName -Unique
     }
+
+    [InstalledApp[]] getCSV() {
+        $items = $this.getAllAppsFromRegistry()        
+        $processedLines = $this.prepareChocoListData()
+
+        return $this.compareChoco($items, $processedLines)
+    }
+
+    [InstalledApp[]] compareChoco([InstalledApp[]] $installedApps, [InstalledApp[]] $processedLines) {
+        $additionalApps = [System.Collections.Generic.List[InstalledApp]]::new()
+
+        #store the lines to reduce processing:
+        $lineSet = @{}
+        foreach ($line in $processedLines) {
+            $lineSet[$line.DisplayName.ToLower().Replace("-", "").Replace("_", "")] = $true
+        }
+
+        foreach ($app in $installedApps) {
+            if ([string]::IsNullOrEmpty($app.DisplayName)) {
+                continue
+            }
+    
+            $lowerDisplayName = $app.DisplayName.ToLower().Replace("-", "").Replace("_", "").split(" ")[0]
+            
+            if ($lineSet.ContainsKey($lowerDisplayName)
+            ) {           
+                $app.Maintainer = "Chocolatey"
+                $lineSet.Remove($lowerDisplayName);
+            }
+
+        }    
+
+
+        foreach ($notUsed in $lineSet.Keys) {
+            $additionalApps.Add([InstalledApp]::new($notUsed, "", "", "Chocolatey"))
+        }
+
+        return $installedApps + $additionalApps.ToArray()
+    }
+
+    [InstalledApp[]] prepareChocoListData() {
+        #run the choco command + parse data into two lists
+        $chocoOut = choco list
+        $lines = $chocoOut -split "`n"  
+        #skip the first and last line, because its the headline and end of the output:
+        $lines = $lines[1..($lines.Length - 2)]
+        
+        $res = $lines | ForEach-Object {
+            $splitted = $_.Split(" ")
+            [InstalledApp]::new($splitted[0], $splitted[1], "", "Chocolatey")
+        }
+        
+        return $res
+    }
+
 }
+
+$manager = [InstalledAppManager]::new()
+
+$allApps = $manager.getCSV()
 
 #export to csv
 $allApps | Export-Csv "finalList.plist" -NoTypeInformation
-
+    
 #add the device name
 $computerName = $env:COMPUTERNAME
 $csvContent = Get-Content "finalList.plist"
 $newContent = @("$computerName") + $csvContent
 $newContent | Set-Content "finalList.plist"
-
+    
 "Done"
